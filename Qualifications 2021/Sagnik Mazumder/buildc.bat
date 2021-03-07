194a70208c473571becc972bb7b9c59820ea0560
Sagnik Mazumder <sagnikmazumdar37@gmail.com>
2021-03-07 23:24:41 +0530

    added code and submission files for extended round along with question pdf, and score screenshots for main and extended rounds

--- Qualifications 2021/Sagnik Mazumder/buildc.bat
+++ Qualifications 2021/Sagnik Mazumder/buildc.bat
@@ -1,2 +1,3 @@
 g++ %1.cc -o %1
 %1 < input/%2.txt > output/%2.out
+del %1.exe
--- Qualifications 2021/Sagnik Mazumder/script.cc
+++ Qualifications 2021/Sagnik Mazumder/script.cc
@@ -1,71 +1,242 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define ld long double
-#define Fill(a, b) memset(a, b, sizeof(a))
-#define IMX 100000
-#define VMX 1000
+#define vi vector<int>
+#define pii pair<int, int>
+#define MAXN 100100
+
+int D, I, S, V, F;
+
+map<string,int> streetIds;
+vector<string> streetNames;
 
 struct Street {
-	int L;
+	int B, E, L;
 	string name;
+	int id;
 
-	void readl() {
-		cin>>name>>L;
+	void read() {
+		cin>>B>>E>>name>>L;
+		id = streetIds.size();
+    	streetIds[name] = id;
+    	streetNames[id] = name;
 	}
 };
 
-unordered_map<int, vector<pair<int, Street>>> streets;
-unordered_map<string, pair<int, int>> street_names;	// street_names[name] = a to b
-vector<string> paths[VMX];
+struct Car {
+  	vi path;
 
-int main(){
-	int D, I, S, V, F;
-	cin>>D>>I>>S>>V>>F;
-	unordered_map<int, pair<int, queue<int>>> in_cars;	// i-th car maps to streets left to take
-	vector<pair<int, pair<int, int>>> in_transit;	// <time, <i-th car, to_intersection>>
-	vector<pair<int, pair<string, int>>> ans;
-
-	// input streets
-	for (int i=0; i<S; ++i) {
-		int b, e;
-		cin>>b>>e;
-		Street s;
-		s.readl();
-		streets[b].emplace_back(make_pair(e, s));
-		street_names[s.name] = make_pair(b, e);
-	}
-
-	// input car paths
-	for (int i=0; i<V; ++i) {
-		int p;
-		cin>>p;
-		for (int j=0; j<p; ++j) {
+  	void read() {
+    	int n;
+    	cin>>n;
+    	path.resize(n);
+	    for (int i = 0; i < n; ++i) {
 			string s;
 			cin>>s;
-			paths[i].emplace_back(s);
-		}
-		in_cars[street_names[paths[i][0]].second].first = street_names[paths[i][0]].first;
-		in_cars[street_names[paths[i][0]].second].second.push(i);
-	}
+    		path[i] = streetIds[s];
+    	}
+  	}
+};
 
-	// run simulation
-	for (int t=0; t<D; ++t) {
+vector<pii> adj[MAXN];
+vector<pii> adji[MAXN];
 
-		for (int i=0; i<in_transit.size(); ++i) {
-			// move car from street to intersection queue if transit time complete
-			if (in_transit[i].first==1){
-				in_cars[street_names[paths[in_transit[i].second.second][0]].second].second.push(in_transit[i].second.first);
-				in_transit.erase(in_transit.begin() + i);
-				--i;
-			}
-			// else reduce transit time
+vector<Street> streets;
+vector<Car> cars;
+
+bool used[MAXN];
+
+vector<vi> simulate() {
+	vector<vi> X(I);
+	for (int i = 0; i < I; ++i) {
+    	int cnt = 0;
+		for (pii v : adji[i]) {
+			if (used[v.second])
+        		cnt++;
+    	}
+    	X[i].resize(cnt, -1);
+  	}
+  	vi Xi(S, -1);
+
+  	// queue for each intersections
+  	vector<queue<int>> q(S);
+
+	// events is "releasing a car from an intersection queue"
+  	// events[t] = queue<street id to process>
+  	vector<queue<int>> events(D+1);
+
+  	// qevents is "a car entering an intersection queue"
+  	// qevents[t] = queue<street id, car id>
+  	vector<queue<pii>> qevents(D+1);
+
+  	// events are triggered by:
+  	// - a car entering a queue
+
+  	// at t = 0, initialize all cars at the end of the streets
+  	vi p(V); // index of car path
+  	for (int i = 0; i < V; ++i) {
+    	int streetId = cars[i].path[p[i]];
+    	qevents[0].emplace(streetId, i);
+  	}
+
+  	vi arrival(V, -1);
+
+  	for (int t = 0; t <= D; ++t) {
+    	// insert cars into queue
+    	while (!qevents[t].empty()) {
+      		pii front = qevents[t].front();
+      		qevents[t].pop();
+
+      		int streetId = front.first;
+      		int carId = front.second;
+      		if (q[streetId].empty()) {
+        		events[t].push(streetId);
+    		}
+      		q[streetId].push(carId);
+    	}
+
+    	while (!events[t].empty()) {
+      		int id = events[t].front();
+      		events[t].pop();
+
+      		queue<int>& qs = q[id];
+	      	assert(!qs.empty());
+
+    		int iid = streets[id].E;
+    		int _t = t;
+    		int cycle = X[iid].size();
+    		int mod = t % cycle;
+      		// check if id is assigned
+      		if (Xi[id] != -1) {
+        		int cycle = X[iid].size();
+        		_t = cycle * (t / cycle) + Xi[id];
+        		if (_t < t) {
+          			_t += cycle;
+        		}
+      		}
+			else if (X[iid][mod] == -1) {
+        		// assign it right away
+        		X[iid][mod] = id;
+        		Xi[id] = mod;
+      		}
 			else {
-				--in_transit[i].first;
-			}
-		}
+        		// defer to t+1
+        		_t = t + 1;
+      		}
 
-		for (int i=0; i<I; ++i)
-	}
-	return (0);
+      		if (t != _t) {
+        		if (_t <= D) {
+          			events[_t].push(id);
+        		}
+      		}
+      		else {
+        		// take single car in the queue
+        		int c = qs.front(); qs.pop();
+        		p[c]++;
+        		assert(p[c] < cars[c].path.size());
+        		int snext = cars[c].path[p[c]];
+        		int inext = streets[snext].E;
+        		int tnext = t + streets[snext].L;
+
+        		if (tnext <= D) {
+          		// arrive
+        			if (p[c] == cars[c].path.size() - 1) {
+            			arrival[c] = tnext;
+          			}
+          			else {
+            			qevents[tnext].emplace(snext, c);
+          			}
+        		}
+
+        		// reinsert self into events if queue is still not empty
+        		if (!qs.empty() && t < D) {
+          			events[t + 1].push(id);
+        		}
+      		}
+    	}
+  	}
+
+  	// assign what's left randomly
+  	int maxslots = 0;
+  	for (int i = 0; i < I; ++i) {
+    	vi slots;
+    	for (int j = 0; j < X[i].size(); ++j) {
+      		if (X[i][j] == -1) {
+        		slots.push_back(j);
+      		}
+    	}
+    	maxslots = max(maxslots, (int)slots.size());
+
+    	for (pii v : adji[i]) {
+      		if (used[v.second] && Xi[v.second] == -1) {
+        		Xi[v.second] = slots.back();
+        		X[i][slots.back()] = v.second;
+        		slots.pop_back();
+      		}
+    	}
+  	}
+  	int score = 0;
+  	for (int c = 0; c < V; ++c) {
+    	if (arrival[c] == -1) continue;
+    	assert(arrival[c] <= D);
+    	score += F + D - arrival[c];
+  	}
+
+  	for (int i = 0; i < I; ++i) {
+    	for (int j = 0; j < X[i].size(); ++j) {
+      		assert(X[i][j] != -1);
+    	}
+  	}
+
+  	return X;
+}
+
+int main() {
+  	cin>>D>>I>>S>>V>>F;
+
+  	streets.resize(S);
+  	streetNames.resize(S);
+  	for (int i = 0; i < S; ++i) {
+    	streets[i].read();
+    	adj[streets[i].B].emplace_back(streets[i].E, streets[i].id);
+    	adji[streets[i].E].emplace_back(streets[i].B, streets[i].id);
+  	}
+
+  	cars.resize(V);
+  	for (int i = 0; i < V; ++i) {
+    	cars[i].read();
+    	for (int s : cars[i].path) {
+      		used[s] = true;
+    	}
+  	}
+
+  	vector<vi> X = simulate();
+
+  	vector<vector<pii>> ans;
+
+  	for (int v = 0; v < I; ++v) {
+    	vector<pii> schedule;
+    	for (int x : X[v]) {
+      		schedule.emplace_back(x, 1);
+    	}
+    	ans.emplace_back(schedule);
+  	}
+  	assert(ans.size() == I);
+
+  	int nans = 0;
+  	for (int v = 0; v < I; ++v) {
+    	if (ans[v].size()) {
+      		nans++;
+    	}
+  	}
+  	printf("%d\n", nans);
+  	for (int v = 0; v < I; ++v) {
+    	if (!ans[v].size()) continue;
+    	printf("%d\n%d\n", v, (int)ans[v].size());
+    	for (pii y : ans[v]) {
+      		printf("%s %d\n", streetNames[y.first].c_str(), y.second);
+    	}
+  	}
+
+  	return 0;
 }
